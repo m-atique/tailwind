f16cd56007a6ef455355caba947b61c418c77b8e
Muhammad Atique <63987802+m-atique@users.noreply.github.com>
2023-01-26 20:36:04 +0500

    applying colors

--- tailwind-series/src/app/page.tsx
+++ tailwind-series/src/app/page.tsx
@@ -1,7 +1,24 @@
 export default function Home() {
   return (
+    <div>
+
     <h1 className="text-3xl font-bold underline">
       Hello world! 
     </h1>
+    {/* //-------------------------------applying colors */}
+    <h1 className="text-green-600">
+      Hello world! 
+    </h1>
+
+    <h1 className="bg-yellow-200">
+      Hello world! 
+    </h1>
+
+    <h1 className="border-4 border-green-400">
+      Hello world! 
+    </h1>
+
+    
+    </div>
   )
 }
