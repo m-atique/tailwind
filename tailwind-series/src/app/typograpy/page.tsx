d883bbc97b64fe39b552e5beb8bbf26968b85e4b
Muhammad Atique <63987802+m-atique@users.noreply.github.com>
2023-01-27 21:41:55 +0500

    add ggole font in global css

--- tailwind-series/src/app/typograpy/page.tsx
+++ tailwind-series/src/app/typograpy/page.tsx
@@ -5,11 +5,11 @@ export default function Typograpy() {
 <Link className ='mb-8 bg-blue-600' href='/'>Home</Link>
    <div className="lg:text-aqeel sm:text-ateeq">
     
-   <h1 className="text-7xl">This is Typograpy h1</h1>
-   <h2 className="text-3xl">This is Typograpy h1</h2>
+   <h1 className="text-7xl mb-4">This is Typograpy h1</h1>
+   <h2 className="font-mono">This is Typograpy h1</h2>
    <h3 className="text-2xl">This is Typograpy h1</h3>
     <p className="text-sx">text with sx </p>
-    <p className="">text with google font </p>
+    <p className="font-mono">text with google font </p>
     <p className="">text with tailwing own fonts </p>   
    </div>
    
