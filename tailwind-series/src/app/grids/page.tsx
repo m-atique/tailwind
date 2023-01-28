7e5e0e899ba2838f980503adeccc6c5c8a114c1a
Muhammad Atique <63987802+m-atique@users.noreply.github.com>
2023-01-28 22:47:46 +0500

    grids management with col-span and row span

--- tailwind-series/src/app/grids/page.tsx
+++ tailwind-series/src/app/grids/page.tsx
@@ -19,36 +19,51 @@ export default function Grids() {
                 </div>
                 </div>
 
-                <div className="bg-yellow-300 rounded-lg ">
-                <div className="">
-                    <div className="">1</div>
-                    <div className="">2</div>
-                    <div className="">3</div>
-                    <div className="">4</div>
-                    <div className="">5</div>
-                    <div className="">6</div>
+                <div className="bg-yellow-300 rounded-lg  ">
+                <div className="grid grid-cols-5 gap-4 p-4">
+                    <div className="bg-pink-200 rounded-md col-start-2 col-end-4  ">col-start-2 col-end-4</div>
+                    <div className="bg-pink-200 rounded-md col-start-3 col-end-5  ">col-start-3 col-end-5</div>
+                    <div className="bg-pink-200 rounded-md col-start-1 col-end-4  ">col-start-1 col-end-4</div>
+                    <div className="bg-pink-200 rounded-md col-start-2 col-end-3  ">col-start-2 col-end-3</div>
+                    <div className="bg-pink-200 rounded-md col-start-1 col-end-5  ">col-start-1 col-end-5</div>
+                    <div className="bg-pink-200 rounded-md ">6</div>
+                    <div className="bg-pink-200 rounded-md ">7</div>
+                    <div className="bg-pink-200 col-span-4 rounded-md ">8</div>
+                    
                 </div>
                 </div>
 
                 <div className="bg-green-300 rounded-lg ">
-                <div className="">
-                    <div className="">1</div>
-                    <div className="">2</div>
-                    <div className="">3</div>
-                    <div className="">4</div>
-                    <div className="">5</div>
-                    <div className="">6</div>
+                <div className="grid grid-flow-col grid-rows-2 p-4 gap-4">
+                    <div className="bg-orange-300 rounded-md row-span-3 ">row-span-3</div>
+                    <div className="bg-orange-300 rounded-md  ">2</div>
+                    <div className="bg-orange-300 rounded-md  ">3</div>
+                    <div className="bg-orange-300 rounded-md  ">4</div>
+                    <div className="bg-orange-300 rounded-md row-span-2 ">row-span2</div>
+                    <div className="bg-orange-300 rounded-md  ">6</div>
+                    <div className="bg-orange-300 rounded-md  ">7</div>
+                    <div className="bg-orange-300 rounded-md  ">8</div>
+                    <div className="bg-orange-300 rounded-md  ">9</div>
+                    <div className="bg-orange-300 rounded-md  ">10</div>
+                    <div className="bg-orange-300 rounded-md row-span-4 ">11 row span 5</div>
+                    <div className="bg-orange-300 rounded-md  ">12</div>
                 </div>
                 </div>
 
                 <div className="bg-rose-300 rounded-lg ">
-                <div className="">
-                    <div className="">1</div>
-                    <div className="">2</div>
-                    <div className="">3</div>
-                    <div className="">4</div>
-                    <div className="">5</div>
-                    <div className="">6</div>
+                <div className="grid grid-flow-row-dense grid-rows-2 p-4 gap-4">
+                    <div className="bg-green-300 rounded-md row-span-3 ">row-span-3</div>
+                    <div className="bg-green-300 rounded-md  ">2</div>
+                    <div className="bg-green-300 rounded-md  ">3</div>
+                    <div className="bg-green-300 rounded-md  ">4</div>
+                    <div className="bg-green-300 rounded-md row-span-3 ">row-span2</div>
+                    <div className="bg-green-300 rounded-md  ">6</div>
+                    <div className="bg-green-300 rounded-md  ">7</div>
+                    <div className="bg-green-300 rounded-md  ">8</div>
+                    <div className="bg-green-300 rounded-md  ">9</div>
+                    <div className="bg-green-300 rounded-md  ">10</div>
+                    <div className="bg-green-300 rounded-md col-span-4 ">11 col span 5</div>
+                    <div className="bg-green-300 rounded-md  ">12</div>
                 </div>
                 </div>
 
