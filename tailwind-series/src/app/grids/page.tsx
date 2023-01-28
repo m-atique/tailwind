dd73013109d2b86f40e7dccf47b923c0448c2ad5
Muhammad Atique <63987802+m-atique@users.noreply.github.com>
2023-01-28 19:43:43 +0500

    setting grids using span

--- tailwind-series/src/app/grids/page.tsx
+++ tailwind-series/src/app/grids/page.tsx
@@ -6,14 +6,16 @@ export default function Grids() {
                 <Link className=' p-4 border-2 rounded-lg border-red-300 bg-blue-600' href='/'>Home</Link>
             </div>
             <div className="grid grid-cols-2 bg-slate-200 gap-4 p-4">
-                <div className="bg-sky-300 rounded-lg ">
-                <div className="">
-                    <div className="">1</div>
-                    <div className="">2</div>
-                    <div className="">3</div>
-                    <div className="">4</div>
-                    <div className="">5</div>
-                    <div className="">6</div>
+                <div className="bg-sky-300 rounded-lg p-5 ">
+                <div className="grid grid-cols-4  gap-3 ">
+                    <div className=" bg-slate-500 rounded-md col-span-3">1</div>
+                    <div className=" bg-slate-500 rounded-md col-span-1">2</div>
+                    <div className=" bg-slate-500 rounded-md col-span-2">3</div>
+                    <div className=" bg-slate-500 rounded-md col-span-2">4</div>
+                    <div className=" bg-slate-500 rounded-md ">5</div>
+                    <div className=" bg-slate-500 rounded-md ">6</div>
+                    <div className=" bg-slate-500 rounded-md ">7</div>
+                    <div className=" bg-slate-500 rounded-md ">8</div>
                 </div>
                 </div>
 
